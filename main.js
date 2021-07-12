79a8ef1e4a8a0fd88b4df8af1c267d5e8d7a3094
Samirah.Alhusayni <Samirah.alhusayni@tuwaiq.edu.sa>
2021-07-12 16:41:12 +0300

    Done

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ $(document).ready(function () {
 
     $("button").on("click", function () {
         let value = $("input").val();
-        let html = '<div class="item">' +
-            '<div class="cancel">X</div>' +
-            value +
-            '</div>';
+        let apiUrl = `https://api.giphy.com/v1/gifs/search?q=hello&limit=1&api_key=8WuA0cvYuycdixljgzQ5VcBzvngl1Sgy`
+        $.ajax(apiUrl).done((res)=> {
+            const urlImage =res.data[0].images.fixed_width_small.url
+            let html = `<img  src ="${urlImage}"class="item">
+             </img> <br/>`;
 
         $("#todo-container").append(html)
+        })
 
 
     })
