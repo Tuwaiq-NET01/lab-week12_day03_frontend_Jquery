eff137dfeba6a122b043635412e24d2cda403bd8
Taghreed <Taghreed.Alqahtani@tuwaiq.edu.sa>
2021-07-14 11:25:40 +0300

    week12 lab

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 $(document).ready(function () {
-
+    
     $("button").on("click", function () {
-        let value = $("input").val();
-        let html = '<div class="item">' +
-            '<div class="cancel">X</div>' +
-            value +
-            '</div>';
+        let word ='https://random-word-api.herokuapp.com/word?number=1';
+        $.ajax(word).done((res)=>{const myword=res[0];
 
-        $("#todo-container").append(html)
+        // let value = $("input").val();
+        let api = `https://api.giphy.com/v1/gifs/search?q=${myword}&limit=1&api_key=lWlGd4qH73vTrdAFuB76srVlJ5rrTluF`
+        $.ajax(api).done((res) => {
+            const url = res.data[0].images.fixed_height_small.url;
+            let html = `<img src="${url}" class="item">` +
+                `<div class="cancel">X</div>` +
+                `</img>` +"<br/>";
+            $("#todo-container").append(html)
+        })})
 
 
     })
