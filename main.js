79e3cf0fbe0701ac40e719b6bc8daec9cd256a27
Abdulmajeed Almaymuni <Abdulmajeed.Almaymuni@tuwaiq.edu.sa>
2021-07-14 14:26:39 +0300

    Fix error

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ $(document).ready(function () {
         console.log("val " +value)
         let api = `https://api.giphy.com/v1/gifs/search?q=${value.toString()}&limit=1&api_key=8WuA0cvYuycdixljgzQ5VcBzvngl1Sgy`
         $.ajax(api).done(function(res){
-            console.log(res.data[0].images.fixed_height_small.url)
             try{
                 let imageUrl = res.data[0].images.fixed_height_small.url
                 let html = '<div class="item">' +
