0cdc67b1cc85c53bcd00dda468e76875c7a2e7d4
Abdulrahman Bin Maneea <Abdulrahman.Bin.Maneea@tuwaiq.edu.sa>
2021-07-14 14:24:53 +0300

    fix: styling and positioning

--- main.js
+++ main.js
@@ -1,32 +1,28 @@
 $(document).ready(function () {
+  $("button").on("click", function () {
+    let word_api = "https://random-word-api.herokuapp.com/word";
+    $.ajax(word_api).done((res) => {
+      let word = res[0];
+      let api = `https://api.giphy.com/v1/gifs/search?q=${word}&limit=1&api_key=IeNzQd68KdNq0kNvMh1mJ73eYTiCT4J2`;
 
-	$("button").on("click", function () {
-		let word_api = "https://random-word-api.herokuapp.com/word"
-		$.ajax(word_api).done((res) => {
-			let word = res[0];
-			let api = `https://api.giphy.com/v1/gifs/search?q=${word}&limit=1&api_key=IeNzQd68KdNq0kNvMh1mJ73eYTiCT4J2`
+      $.ajax(api).done((res) => {
+        if (res.data.length <= 0) {
+          $("button").click();
+        } else {
+          let path = res.data[0].images.fixed_width.url;
+          let html =
+            '<div class="item">' +
+            '<div class="cancel">X</div><h1>' +
+            word +
+            `</h1><img src="${path}">` +
+            "</div>";
+          $("#todo-container").append(html);
+        }
+      });
+    });
+  });
 
-			$.ajax(api).done((res) => {
-				let path = res.data[0].images.fixed_width.url
-				console.log(path)
-				let html = '<div class="item">' +
-					'<div class="cancel">X</div>' +
-					word +
-					`<img src="${path}">` +
-					'</div>'
-				;
-
-				$("#todo-container").append(html)
-			})
-
-		})
-
-
-	})
-
-	$("#todo-container").on("click", ".cancel", function () {
-		$(this).parent().remove();
-	})
-
-
-})
+  $("#todo-container").on("click", ".cancel", function () {
+    $(this).parent().remove();
+  });
+});
