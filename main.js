a57aae86d6d0ae965daddef04cefad854a3d48b1
HassanAlgadah <h--san@hotmail.com>
2021-07-14 10:39:58 +0300

    hassan

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 $(document).ready(function () {
 
     $("button").on("click", function () {
+        let url
         let value = $("input").val();
-        let html = '<div class="item">' +
-            '<div class="cancel">X</div>' +
-            value +
-            '</div>';
-
-        $("#todo-container").append(html)
+        $.ajax("https://random-word-api.herokuapp.com/word?number=1").done((res) => {
+            let api = `https://api.giphy.com/v1/gifs/search?q=${res[0]}&limit=1&api_key=lWlGd4qH73vTrdAFuB76srVlJ5rrTluF`
+            $.ajax(api).done(res => {
+                url = (res.data[0].images.fixed_height_downsampled.url)
+                let html = `<img src=${url} class="item">` +
+                    '<div class="cancel">X</div>' +
+                    value +
+                    '';
+                $("#todo-container").append(html)
+            })
+        })
 
 
     })
