4fdae9212d7a530cb813ab69bce23d76d05c0c13
unknown <Fatimah.Alqhtany@tuwaiq.edu.sa>
2021-07-14 14:18:46 +0300

    first commit

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 $(document).ready(function () {
 
-    $("button").on("click", function () {
+    $("#add-btn").on("click", function () {
+
         let value = $("input").val();
-        let html = '<div class="item">' +
-            '<div class="cancel">X</div>' +
-            value +
-            '</div>';
+        let api = `https://api.giphy.com/v1/gifs/search?q=${value}&limit=1&api_key=lWlGd4qH73vTrdAFuB76srVlJ5rrTluF`
 
-        $("#todo-container").append(html)
-
-
+        $.ajax(api).done((res) => {
+            let API = `<div class="item"> <div class="cancel">X</div> ${value}` +
+                `<img src=${res.data[0].images.fixed_height.url}/>  </div>`;
+            $("#todo-container").append(API)
+        })
     })
+    $("#random-btn").on("click", () => {
+        let randomApi = '';
+        let randomWordAPI = "https://random-word-api.herokuapp.com/word?number=1";
 
+        $.ajax(randomWordAPI).done((res) => {
+           
+            randomApi = res[0];
+            let api = `https://api.giphy.com/v1/gifs/search?q=${randomApi}&limit=1&api_key=lWlGd4qH73vTrdAFuB76srVlJ5rrTluF`
+            $.ajax(api).done((res) => {
+             
+                let API = `<div class="item"> <div class="cancel">X</div> ${randomApi}` +
+                    `<img src=${res.data[0].images.fixed_height.url}/>  </div>`;
+                $("#todo-container").append(API)
+            })
+        })
+    });
     $("#todo-container").on("click", ".cancel", function () {
         $(this).parent().remove();
     })
-
-
 })
