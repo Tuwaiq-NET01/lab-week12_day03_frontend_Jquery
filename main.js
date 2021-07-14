34cf1d9e1725c80ac6a634b3ed33bd32f7295f1e
AfraAllahyani <Afra.Allahyani@tuwaiq.edu.sa>
2021-07-14 10:59:44 +0300

    done

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 $(document).ready(function () {
-
     $("button").on("click", function () {
-        let value = $("input").val();
-        let html = '<div class="item">' +
-            '<div class="cancel">X</div>' +
-            value +
-            '</div>';
-
-        $("#todo-container").append(html)
-
-
+        let word_api = "https://random-word-api.herokuapp.com/word"
+        $.ajax(word_api).done((res) => {
+            let word = res[0];
+            let api = `https://api.giphy.com/v1/gifs/search?q=${word}&limit=1&api_key=IeNzQd68KdNq0kNvMh1mJ73eYTiCT4J2`
+            $.ajax(api).done((res) => {
+                let path = res.data[0].images.fixed_width.url
+                console.log(path)
+                let html = '<div class="item">' +
+                    '<div class="cancel">X</div>' +
+                    word +
+                    `<img src="${path}">` +
+                    '</div>';
+                $("#todo-container").append(html)
+            })
+        })
     })
-
     $("#todo-container").on("click", ".cancel", function () {
-        $(this).parent().remove();
-    })
-
-
+        $(this).parent().remove(); })
 })
