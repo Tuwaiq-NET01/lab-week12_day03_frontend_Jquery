1acef269dde67e106c8f1b9961dfdf122d0bbfc2
Reema3Alyousef <reema.alyousef@tuwaiq.edu.sa>
2021-07-14 14:33:47 +0300

    submitted

--- main.js
+++ main.js
@@ -2,13 +2,20 @@ $(document).ready(function () {
 
     $("button").on("click", function () {
         let value = $("input").val();
-        let html = '<div class="item">' +
-            '<div class="cancel">X</div>' +
-            value +
-            '</div>';
+        let apiword = "https://random-word-api.herokuapp.com/word?number=1"
+        $.ajax(apiword).done((res)=>{
+           var wrr= res[0]
+           let api = `https://api.giphy.com/v1/gifs/search?q=${wrr}&limit=1&api_key=lWlGd4qH73vTrdAFuB76srVlJ5rrTluF`
+           $.ajax(api).done((res)=>{
+           const url =res.data[0].images.downsized_large.url;
+           let html = '<div class="item">' +
+              '<div class="cancel">X</div>' +
+              `<img src=${url}/> `
+              '</div>';
+              $("#todo-container").append(html)
+           })
 
-        $("#todo-container").append(html)
-
+        })
 
     })
 
