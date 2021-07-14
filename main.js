b3ce4e3919402682678ece1ff47e6684e5bf5528
Albandry AlQaseemi <Albandr.alqaseemi@gmail.com>
2021-07-14 15:04:03 +0300

    Add Random and search Giphy

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 $(document).ready(function () {
-    $("button").on("click", function () {
+    $("Button").on("click", function () {
         let value = $("input").val();
+        let apiword = "https://random-word-api.herokuapp.com/word?number=1"
+        $.ajax(apiword).done((res)=>{
+           var wrr= res[0]
+           let api = `https://api.giphy.com/v1/gifs/search?q=${value}&limit=1&api_key=lWlGd4qH73vTrdAFuB76srVlJ5rrTluF`
+           $.ajax(api).done((res)=>{
+           const url =res.data[0].images.downsized_large.url;
+           let html = '<div class="item">' +
+              '<div class="cancel"><i class="fa fa-trash"></i></div>' +
+              `<img src=${url}/> `
+              '</div>';
+              $("#todo-container").append(html)
+           })
+        })
+    })
+    $("Button").on("click", function () {
         let apiword = "https://random-word-api.herokuapp.com/word?number=1"
         $.ajax(apiword).done((res)=>{
            var wrr= res[0]
