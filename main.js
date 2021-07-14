d05a1a1478b27a68de6880a6125e2037a8e50da7
meshal <meshal.najdi@tuwaig.edu.sa>
2021-07-14 10:52:19 +0300

    Async

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
 $(document).ready(function () {
+    $(document).ready(function() {
+        $('button').on('click', function() {
+            let value = $('input').val();
+            let word = 'https://random-word-api.herokuapp.com/word?number=1';
+            $.ajax(word).done((res) => {
+                let api = `https://api.giphy.com/v1/gifs/search?q=${res[0]}&limit=1&api_key=lWlGd4qH73vTrdAFuB76srVlJ5rrTluF`;
+                $.ajax(api).done((res) => {
+                    const url = res.data[0].images.fixed_height_still.url;
+                    let html = `<img src=${url} class="item"><div class="cancel">X</div></img> <br/>`;
+                    $('#todo-container').append(html);
+                });
+            });
+        });
+    
+        $("button").on("click", function () {
+            let value = $("input").val();
+            let html = '<div class="item">' +
+                '<div class="cancel">X</div>' +
+                value +
+                '</div>';
+    
+            $("#todo-container").append(html)
+    
+    
+        })
+    
+        $("#todo-container").on("click", ".cancel", function () {
+        $('#todo-container').on('click', '.cancel', function() {
+            $(this).parent().remove();
+        })
 
-    $("button").on("click", function () {
-        let value = $("input").val();
-        let html = '<div class="item">' +
-            '<div class="cancel">X</div>' +
-            value +
-            '</div>';
 
-        $("#todo-container").append(html)
-
-
-    })
-
-    $("#todo-container").on("click", ".cancel", function () {
-        $(this).parent().remove();
-    })
-
-
-})
+    }) 
+});
+});     
