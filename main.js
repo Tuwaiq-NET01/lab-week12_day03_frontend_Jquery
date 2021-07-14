341858c48ea4a99043a40fbb4157e0c2494da239
RawabeT <rawabe44u@gmail.com>
2021-07-14 14:33:15 +0300

    RandomWords

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 $(document).ready(function () {
 
-    $("button").on("click", function () {
+    $("button").on("click",  function () {
         let value = $("input").val();
-        let html = '<div class="item">' +
-            '<div class="cancel">X</div>' +
-            value +
-            '</div>';
+        let randomWord = `https://random-word-api.herokuapp.com/word?number=1`
+        $.ajax( randomWord).done((res) => {
+            const word = res[0]
 
-        $("#todo-container").append(html)
-
-
+            let api = `https://api.giphy.com/v1/gifs/search?q=${word}&limit=1&api_key=8WuA0cvYuycdixljgzQ5VcBzvngl1Sgy`
+            $.ajax(api).done((res) => {
+                const imgUrl = res.data[0].images.fixed_width_small.url
+                let html = `<img class="item" src=${imgUrl}>` +
+                    '<div class="cancel">X</div>' +
+                    value +
+                    '</img>';
+    
+                $("#todo-container").append(html)
+            })
+        })
     })
 
     $("#todo-container").on("click", ".cancel", function () {
